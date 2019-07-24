8bbe2550692494a17b71b286e05063b2e548da34
Jiří Martinů <jiri.martinu@email.cz>
2019-07-24 22:21:31 +0200

    allow action use custom template

--- src/Column/Action.php
+++ src/Column/Action.php
@@ -9,90 +9,111 @@ namespace FreezyBee\DataGridBundle\Column;
  */
 class Action
 {
-    /** @var string */
-    private $route;
+	/** @var string */
+	private $route;
 
-    /** @var string */
-    private $label;
+	/** @var string */
+	private $label;
 
-    /** @var string|null */
-    private $icon;
+	/** @var string|null */
+	private $icon;
 
-    /** @var string|null */
-    private $class;
+	/** @var string|null */
+	private $class;
 
-    /** @var array */
-    private $params;
+	/** @var array */
+	private $params;
 
-    /**
-     * @param string $route
-     * @param string $label
-     * @param array $params
-     */
-    public function __construct(string $route, string $label, array $params)
-    {
-        $this->route = $route;
-        $this->label = $label;
-        $this->params = $params;
-    }
+	/** @var null|string */
+	private $template;
 
-    /**
-     * @return string
-     */
-    public function getRoute(): string
-    {
-        return $this->route;
-    }
+	/**
+	 * @param string $route
+	 * @param string $label
+	 * @param array $params
+	 */
+	public function __construct(string $route, string $label, array $params)
+	{
+		$this->route = $route;
+		$this->label = $label;
+		$this->params = $params;
+	}
 
-    /**
-     * @return string
-     */
-    public function getLabel(): string
-    {
-        return $this->label;
-    }
+	/**
+	 * @return string
+	 */
+	public function getRoute(): string
+	{
+		return $this->route;
+	}
 
-    /**
-     * @return array
-     */
-    public function getParams(): array
-    {
-        return $this->params;
-    }
+	/**
+	 * @return string
+	 */
+	public function getLabel(): string
+	{
+		return $this->label;
+	}
 
-    /**
-     * @return string|null
-     */
-    public function getIcon(): ?string
-    {
-        return $this->icon;
-    }
+	/**
+	 * @return array
+	 */
+	public function getParams(): array
+	{
+		return $this->params;
+	}
 
-    /**
-     * @param string|null $icon
-     * @return Action
-     */
-    public function setIcon(?string $icon): self
-    {
-        $this->icon = $icon;
-        return $this;
-    }
+	/**
+	 * @return string|null
+	 */
+	public function getIcon(): ?string
+	{
+		return $this->icon;
+	}
 
-    /**
-     * @return string|null
-     */
-    public function getClass(): ?string
-    {
-        return $this->class;
-    }
+	/**
+	 * @param string|null $icon
+	 * @return Action
+	 */
+	public function setIcon(?string $icon): self
+	{
+		$this->icon = $icon;
+		return $this;
+	}
 
-    /**
-     * @param string|null $class
-     * @return Action
-     */
-    public function setClass(?string $class): self
-    {
-        $this->class = $class;
-        return $this;
-    }
+	/**
+	 * @return string|null
+	 */
+	public function getClass(): ?string
+	{
+		return $this->class;
+	}
+
+	/**
+	 * @param string|null $class
+	 * @return Action
+	 */
+	public function setClass(?string $class): self
+	{
+		$this->class = $class;
+		return $this;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getTemplate(): ?string
+	{
+		return $this->template;
+	}
+
+	/**
+	 * @param string|null $template
+	 * @return Action
+	 */
+	public function setTemplate(?string $template): Action
+	{
+		$this->template = $template;
+		return $this;
+	}
 }
